293660cfe262107e9903cbda83d4d9735ed01d35
Borbola <borbolapatrik@gmail.com>
2024-11-18 12:25:51 +0100

    Add project files.

---
+++ HotelAPI/Entities/Booking.cs
@@ -0,0 +1,15 @@
+﻿
+namespace HotelAPI.Entities
+{
+    public class Booking
+    {
+        public int Id { get; set; }
+        public int GuestId { get; set; }
+        public int RoomId { get; set; }
+        public DateTime CheckInDate { get; set; }
+        public DateTime? CheckOutDate { get; set; }
+
+        public virtual Guest? Guest { get; set; }
+        public virtual Room? Room { get; set; }
+    }
+}
